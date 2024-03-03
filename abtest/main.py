f8f2fbeef7642d2197d4eed048ed9cbabce795b4
ghaefner <guillaume-haefner@gmx.de>
2024-03-03 18:43:54 +0100

    Update main module

--- abtest/main.py
+++ abtest/main.py
@@ -4,4 +4,4 @@ from src.analyzer import ABTestAnalyzer
 analyzer = ABTestAnalyzer(file_path="abtest/data/ab_data.csv")
 
 analyzer.print_data_info()
-analyzer.run_abtest(N_sample=4000)
+analyzer.run_all(N_sample=4000)
